18919bbe63379c309cd7f7d1bbf2723f85b1c3a9
ovidiu <ovidiu@pascut.com>
2018-04-09 15:19:03 -0400

    changed folder structure

--- photo-organizer.py
+++ photo-organizer.py
@@ -5,13 +5,19 @@ import shutil
 import hashlib
 import exifread
 from datetime import datetime
+import re
 
 
-FILE_TYPES_TO_PROCESS = ('.jpg', '.jpeg', '.mov', '.avi', '.mp4', '.cr2', '.3gp', '.wmv', '.mpg', '.m4v', '.png')
-FILES_TO_DELETE = ('.picasa.ini', 'picasa.ini', '.ds_store', '._.ds_store', 'thumbs.db', 'zbthumbnail.info')
+FILE_TYPES_TO_PROCESS = ('.jpg', '.jpeg', '.mov', '.avi',
+                         '.mp4', '.cr2', '.3gp', '.wmv', '.mpg', '.m4v', '.png')
+FILES_TO_DELETE = ('.picasa.ini', 'picasa.ini', '.ds_store',
+                   '._.ds_store', 'thumbs.db', 'zbthumbnail.info')
 DATETIME_EXIF_TAG = 'Image DateTime'
 DATETIME_EXIF_TAG_FORMAT = '%Y:%m:%d %H:%M:%S'
 FILE_DUP_MAX_RETRIES = 50
+
+RE_DATE = re.compile(r'.*?\D*(20\d{6})\D.*')
+
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -20,121 +26,121 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def get_files(path):
-	for i in sorted(os.listdir(path)):
-		p = os.path.join(path, i)
-		if os.path.isdir(p):
-			for f in get_files(p):
-				yield f
-		else:
-			yield p
+    for i in sorted(os.listdir(path)):
+        p = os.path.join(path, i)
+        if os.path.isdir(p):
+            for f in get_files(p):
+                yield f
+        else:
+            yield p
+
 
 def remove_empty_dirs(path):
-	for i in sorted(os.listdir(path)):
-		p = os.path.join(path, i)
-		if os.path.isdir(p):
-			remove_empty_dirs(p)
-			if not os.listdir(p):
-				print("removing empty dir {0}".format(p))
-				os.rmdir(p)
+    for i in sorted(os.listdir(path)):
+        p = os.path.join(path, i)
+        if os.path.isdir(p):
+            remove_empty_dirs(p)
+            if not os.listdir(p):
+                print("removing empty dir {0}".format(p))
+                os.rmdir(p)
+
 
 def get_file_date(path):
-	f = open(path, 'rb')
-	tags = exifread.process_file(f, details=False)
-	if DATETIME_EXIF_TAG in tags:
-		dt_str = str(tags[DATETIME_EXIF_TAG])
-		try:
-			return datetime.strptime(dt_str, DATETIME_EXIF_TAG_FORMAT)
-		except ValueError as e:
-			return None
-	else:
-		ts = os.stat(path).st_mtime
-		return datetime.fromtimestamp(ts)
-	return None
+    file_name = os.path.splitext(os.path.basename(path))[0]
+    f = open(path, 'rb')
+    tags = exifread.process_file(f, details=False)
+    if DATETIME_EXIF_TAG in tags:
+        dt_str = str(tags[DATETIME_EXIF_TAG])
+        try:
+            return datetime.strptime(dt_str, DATETIME_EXIF_TAG_FORMAT)
+        except ValueError as e:
+            print(e)
+    m = RE_DATE.match(file_name)
+    if m:
+        return datetime.strptime(m.groups()[0], '%Y%m%d')
+    else:
+        ts = os.stat(path).st_mtime
+        return datetime.fromtimestamp(ts)
+    return None
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Organize photo library')
-	parser.add_argument('src', help='source folder')
-	parser.add_argument('dest', help='destination folder')
-	parser.add_argument('-c', '--copy-only', 
-						action='store_true',
-						help='copy files instead of moving')
+    parser = argparse.ArgumentParser(description='Organize photo library')
+    parser.add_argument('src', help='source folder')
+    parser.add_argument('dest', help='destination folder')
+    parser.add_argument('-c', '--copy-only',
+                        action='store_true',
+                        help='copy files instead of moving')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	src = os.path.normpath(args.src)
-	dest = os.path.normpath(args.dest)
+    src = os.path.normpath(args.src)
+    dest = os.path.normpath(args.dest)
 
-	if not os.path.exists(src):
-		print("source path {0} does not exists".format(src))
-		exit(0)
+    if not os.path.exists(src):
+        print("source path {0} does not exists".format(src))
+        exit(0)
 
-	if not os.path.exists(dest):
-		print("destination path {0} does not exists".format(dest))
-		exit(0)
+    if not os.path.exists(dest):
+        print("destination path {0} does not exists".format(dest))
+        exit(0)
 
-	if os.stat(src) == os.stat(dest):
-		print("cannot use same path {0} as source and destination".format(src))
-		exit(0)	
+    if os.stat(src) == os.stat(dest):
+        print("cannot use same path {0} as source and destination".format(src))
+        exit(0)
 
-	for f in get_files(src):
-		_, ext = os.path.splitext(f)
-		ext = ext.lower()
-		if ext not in FILE_TYPES_TO_PROCESS:
-			if os.path.basename(f).lower() in FILES_TO_DELETE:
-				if not args.copy_only:
-					print("deleting {0}".format(f))
-					os.remove(f)
-				continue
-			else:
-				if args.copy_only:
-					print("skipping {0}, not a file type to copy".format(f))
-				else:
-					print("skipping {0}, not a file type to move".format(f))
-				continue
-		d = get_file_date(f)
-		if not d:
-			print("skipping {0}, no date found".format(f))
-			continue
-		dest_folder = os.path.join(dest, 
-									str(d.year), 
-									"{0:02}".format(d.month), 
-									"{0:02}".format(d.day)									
-									)
-		if not os.path.exists(dest_folder):
-			os.makedirs(dest_folder)
-		dest_path = os.path.join(dest_folder, os.path.split(f)[1])
-		if os.path.exists(dest_path):
-			if os.stat(f).st_size == os.stat(dest_path).st_size and \
-				md5(f) == md5(dest_path):
-				print("skipping {0}, already exists as {1} with same size and hash".format(f, dest_path))
-				if not args.copy_only:
-					os.remove(f)
-				continue
-			else:
-				counter = 1
-				new_path = dest_path
-				while os.path.exists(new_path) and counter < FILE_DUP_MAX_RETRIES:
-					new_path = ("-" + str(counter)).join(os.path.splitext(dest_path))
-					counter += 1
-				if counter < FILE_DUP_MAX_RETRIES:
-					dest_path = new_path
-				else:
-					print("error: cannot copy {0}, already exists as {1}".format(f, new_path))
+    for f in get_files(src):
+        _, ext = os.path.splitext(f)
+        ext = ext.lower()
+        if ext not in FILE_TYPES_TO_PROCESS:
+            if os.path.basename(f).lower() in FILES_TO_DELETE:
+                if not args.copy_only:
+                    print("deleting {0}".format(f))
+                    os.remove(f)
+                continue
+            else:
+                if args.copy_only:
+                    print("skipping {0}, not a file type to copy".format(f))
+                else:
+                    print("skipping {0}, not a file type to move".format(f))
+                continue
+        d = get_file_date(f)
+        if not d:
+            print("skipping {0}, no date found".format(f))
+            continue
+        dest_folder = os.path.join(dest, "{0}-{1:02}".format(d.year, d.month))
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+        dest_path = os.path.join(dest_folder, os.path.split(f)[1])
+        if os.path.exists(dest_path):
+            if os.stat(f).st_size == os.stat(dest_path).st_size and \
+                    md5(f) == md5(dest_path):
+                print("skipping {0}, already exists as {1} with same size and hash".format(
+                    f, dest_path))
+                if not args.copy_only:
+                    os.remove(f)
+                continue
+            else:
+                counter = 1
+                new_path = dest_path
+                while os.path.exists(new_path) and counter < FILE_DUP_MAX_RETRIES:
+                    new_path = ("-" + str(counter)
+                                ).join(os.path.splitext(dest_path))
+                    counter += 1
+                if counter < FILE_DUP_MAX_RETRIES:
+                    dest_path = new_path
+                else:
+                    print("error: cannot copy {0}, already exists as {1}".format(
+                        f, new_path))
 
-		if args.copy_only:
-			print("copying {0} to {1}".format(f, dest_path))
-			shutil.copy2(f, dest_path)
-		else:
-			print("moving {0} to {1}".format(f, dest_path))
-			os.rename(f, dest_path)
+        if args.copy_only:
+            print("copying {0} to {1}".format(f, dest_path))
+            shutil.copy2(f, dest_path)
+        else:
+            print("moving {0} to {1}".format(f, dest_path))
+            os.rename(f, dest_path)
 
-	if not args.copy_only:
-		remove_empty_dirs(src)
-
-
-
-
-
-
+    if not args.copy_only:
+        remove_empty_dirs(src)
